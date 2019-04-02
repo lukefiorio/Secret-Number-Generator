545f782d5f12819121488465f12f2c398f124644
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-01 18:25:30 -1000

    passing 3 tests

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
 module.exports = function() {
-  
+  // set secretNumber to a random Number
+  const secretNumber = Math.random()*1000000;
+  // declare a function that will return that secretNumber when invoked
+  const secretNumberGenerator = function() {
+    return secretNumber;
+  }
+  // return (and therefore invoke) the function that will return the secretNumber var
+  return secretNumberGenerator;
 };
